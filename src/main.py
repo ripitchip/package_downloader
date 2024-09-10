919b7a0a63ff629731f29050013cc30f8e2663aa
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-10 16:51:13 +0200

    :sparkles: feat: #2 Creation endpoint PIP

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 
 # Directory where RPMs and tar.gz files are stored
 RPM_DIR = Path("/tmp/rpm_dir")
+PIP_DIR = Path("/tmp/pip_dir")
 
 
 @app.post("/rpm/")
@@ -77,18 +78,68 @@ def create_package_yum(pkg_name: str | list[str]) -> FileResponse:
             detail="Error occurred while creating the package tar file.",
         ) from err
 
+
+@app.post("/pip/")
+def create_package_pip(pkg_name: str | list[str]) -> FileResponse:
+    """
+    Create a tar.gz file for the specified python package(s).
+
+    Args:
+    ----
+    pkg_name (Union[str, List[str]]): The name of the package to download.
+
+    Returns:
+    -------
+    FileResponse: A response with the tar.gz file for download.
+
+    """
+    if isinstance(pkg_name, str):
+        pkg_name = [
             pkg_name,
-        ],
-        check=True,
-    )
+        ]  # Convert single package name to a list for uniform handling
 
-    # Check if the tar file was created successfully
-    if not os.path.exists(tar_file):
-        raise HTTPException(status_code=500, detail="Failed to create tar file")
+    package_dirs = []
+    try:
+        for pkg in pkg_name:
+            package_dir = PIP_DIR / pkg
+            package_dir.mkdir(parents=True, exist_ok=True)
+            package_dirs.append(package_dir)
 
-    # Return the tar.gz file as a response
-    return FileResponse(
-        tar_file,
-        media_type="application/gzip",
-        filename=os.path.basename(tar_file),
-    )
+            # Download the package using yumdownloader
+            subprocess.run(
+                [
+                    "pip3.12",
+                    "download",
+                    "--dest",
+                    f"{package_dir}",
+                    pkg,
+                ],
+                check=True,
+            )
+
+        # Create a tar.gz file with all downloaded packages
+        tar_file = PIP_DIR / "packages.tar.gz"
+        subprocess.run(
+            ["tar", "-czvf", str(tar_file), "-C", str(PIP_DIR)]
+            + [str(d.name) for d in package_dirs],
+            check=True,
+        )
+
+        # Check if the tar file was created successfully
+        if not tar_file.exists():
+            raise HTTPException(
+                status_code=500,
+                detail="Failed to create tar file",
+            )
+
+        # Return the tar.gz file as a response
+        return FileResponse(
+            path=str(tar_file),
+            media_type="application/gzip",
+            filename=tar_file.name,
+        )
+    except subprocess.CalledProcessError as err:
+        raise HTTPException(
+            status_code=500,
+            detail="Error occurred while creating the package tar file.",
+        ) from err
